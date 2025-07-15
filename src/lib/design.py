83ece3fd8c4c269244139bcef36a04fb1c42c1a2
London Bielicke <bielickelondon@gmail.com>
2025-07-14 18:36:28 -0700

    decoupled latex formatter class

--- src/lib/design.py
+++ src/lib/design.py
@@ -13,6 +13,7 @@ from lib.candl import generate_conditions
 import math
 import pandas as pd
 from lib.randomizer import Randomizer
+from lib.formatter import LatexExport
 
 class Plans:
     def __init__(self):
@@ -69,18 +70,10 @@ class Design(Plans):
         self.rank_constraints = {}
   
     def to_latex(self):
-        matrix = self.eval()
-        trials = [f"trial{i+1}" for i in range(len(matrix[0]))]
-        df = pd.DataFrame(matrix, columns=trials)
-
-        try:
-            filepath = "../outputs/design.tex"
-            create_directory_for_file(filepath)
-            with open(filepath, 'w', encoding='utf-8') as tex_file:
-                tex_file.write(df.to_latex())
-            print(f"success")
-        except Exception as e:
-            print(f"An error occurred: {e}")
+        # FIXME: won't work with random plans
+        matrix = self.get_plans()
+        formatter = LatexExport(matrix)
+        formatter.to_latex()
 
     def num_trials(self, n):
         self.trials = n
@@ -112,10 +105,6 @@ class Design(Plans):
     def has_random_variable(self):
         return self.random_var is not None
     
-
-    def _generate_random_variables(self, n, variable, width):
-        ws_conditions = generate_conditions(n, variable, width)
-        return ws_conditions
     
     def _determine_random_width(self):
         width = self.get_width()
@@ -138,43 +127,6 @@ class Design(Plans):
                span = constraint.width
         return span
     
-    def get_blocks(self, plan, width, span):
-        """Chunk the plan into blocks of size width * span."""
-        block_size = width * span
-        return [plan[i:i + block_size] for i in range(0, len(plan), block_size)]
-
-    def replace_condition(self, condition_str, replacement, index_to_replace):
-        """Replace part of a condition string at the given index."""
-        parts = condition_str.split("-")
-        parts[index_to_replace] = replacement
-        return "-".join(parts)
-
-    def apply_randomization(self, rand_vars, width, span, random_index, n):
-        new_plans = []
-        for plan_idx, plan in enumerate(self.plans):
-            blocks = self.get_blocks(plan, width, span)
-            reps_per_plan = int(n / len(self.plans))
-
-            for rep_idx in range(reps_per_plan):
-                new_plan = []
-
-                for block_idx, block in enumerate(blocks):
-                    # FIXME: does  not work when random variables are the outer design
-                    rand_idx = rep_idx * len(blocks) + plan_idx * n + block_idx
-                    rand_var = rand_vars[rand_idx]
-                    for cond_idx in range(width):
-                        for within_block_idx in range(span):
-                            index = cond_idx * span + within_block_idx
-                            old_condition = block[index]
-                            replacement = rand_var[cond_idx]
-                            new_condition = self.replace_condition(old_condition, replacement, random_index)
-                            new_plan.append(new_condition)
-
-                new_plans.append(new_plan)
-
-        self.plans = new_plans
-
-
     def _instantiate_random_variables(self, n):
         # NOTE to self: this will only work if there is one random variable :)
         """
